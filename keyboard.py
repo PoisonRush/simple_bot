d0fcab5b15c94d407959884db89ccecef73d3ea5
PoisonRush <volf.vladimir2010@yandex.ru>
2025-05-27 18:05:33 +0300

    Changed keyboard

--- keyboard.py
+++ keyboard.py
@@ -12,21 +12,17 @@ def get_start_kb():
 def get_base_level_kb():
     """Кнопки для базового уровня"""
     buttons = [
-        [InlineKeyboardButton(text='Назад', callback_data='back')],
-        [
         [InlineKeyboardButton(text='1. Объявление переменных', callback_data='variables')],
         [InlineKeyboardButton(text='2. Типы данных', callback_data='datatypes')],
         [InlineKeyboardButton(text='3. Операторы', callback_data='operators')],
-        ],
     ]
     return InlineKeyboardMarkup(inline_keyboard=buttons)
 
 def get_medium_level_kb():
     """Кнопки для среднего уровня"""
     buttons = [
-        [InlineKeyboardButton(text='Назад', callback_data='back')],
-        [[InlineKeyboardButton(text='1. Функции', callback_data='functions')],
+        [InlineKeyboardButton(text='1. Функции', callback_data='functions')],
         [InlineKeyboardButton(text='2. Файлы', callback_data='files')],
-        [InlineKeyboardButton(text='3. Модули', callback_data='modules')],],
+        [InlineKeyboardButton(text='3. Модули', callback_data='modules')],
     ]
     return InlineKeyboardMarkup(inline_keyboard=buttons)
